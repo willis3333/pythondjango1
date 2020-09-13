90087b481b8ec84e29f548c8d179648390fd4a28
willis3333 <60554260+willis3333@users.noreply.github.com>
2020-09-13 17:16:53 -0400

    update wsgi for web app

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+from helloworld.wsgi import HelloWorldApplication
+application = HelloWorldApplication(application)
